a0a6b9ab4fffa2805393ea0c56e4f192ebea17f0
Mfetko705 <145793105+Mfetko705@users.noreply.github.com>
2024-04-29 22:54:52 -0400

    Add files via upload

--- projectJS.js
+++ projectJS.js
@@ -1,61 +1,46 @@
-let myInfo = document.getElementById("infoDiv");
-let p1 = document.createElement("p");
 
-let node1 = document.createTextNode("Fill out your information:");
-
-p1.appendChild(node1);
-myInfo.appendChild(p1);
-
-//let ul = document.createElement('ul');
-
-let nameLabel= document.createElement("name");
-nameLabel.innerHTML= "Enter your name ";
-myInfo.appendChild(nameLabel);
-
-let nameBox= document.createElement("input");
-nameBox.type= "text";
-myInfo.appendChild(nameBox);
-
-//let break1= document.createElement("br")
-//nameBox.appendChild(break1);
-
-let emailLabel= document.createElement("email");
-emailLabel.innerHTML= "Enter your email address ";
-myInfo.appendChild(emailLabel);
-
-let emailBox= document.createElement("input");
-emailBox.type= "text";
-myInfo.appendChild(emailBox);
-
-//document.appendChild(document.createElement("br"));
-
-let goalLabel= document.createElement("goal");
-goalLabel.innerHTML= "What is your goal this week?  ";
-myInfo.appendChild(goalLabel);
-
-let goalBox= document.createElement("input");
-goalBox.type= "text";
-myInfo.appendChild(goalBox);
-
-//function- validate email and populate meal plan inputs?
-let submitButton = document.createElement("button");
-submitButton.textContent = "Submit";
-myInfo.appendChild(submitButton);
+let submit = document.getElementById("submit");
 
 
+//infoForm.addEventListener("submit", checkEmail);
+//let submit = document.getElementById("submit");
+
+submit.addEventListener("click", checkEmail)
+
+function checkEmail() {
+    let emailInput = document.getElementById("emailInput").value;
+    //let mailRegex = "^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$"
+    let str = /^[^\s@]+@[^\s@]+\.[^\s@]/;
+
+    let mailRegex = RegExp(str);
+    let valid = str.test(emailInput);
+
+    if (emailInput= "") {
+        window.alert("Please enter your email address!")
+    } else if (!valid){
+        window.alert("Please enter a valid email address!");
+
+} else {
+    showPlanner()
+    }
+    }
+    
+    function showPlanner(){
+    document.getElementById("emailInput").value = "";
+    let myInfo = document.getElementById ("scheduleDiv");
+    myInfo.innerHTML = "";
 
 
+let title = document.createElement("h2");
+title.textContent = "Create your meal plan!"
+myInfo.appendChild(title);
 
+let p5 = document.createElement("p");
 
-let mySchedule = document.getElementById("scheduleDiv");
-//mySchedule.appendChild(h1);
+let node5 = document.createTextNode("Sunday");
 
-let p2 = document.createElement("p");
-
-let node2 = document.createTextNode("Fill out your weekly meal plan!");
-
-p2.appendChild(node2);
-mySchedule.appendChild(p2);
+p5.appendChild(node5);
+myInfo.appendChild(p5);
 
 let ul = document.createElement('ul');
 
@@ -77,9 +62,16 @@ for (let i = 0; i< labelSunday.length; i++){
     liSun.appendChild(sunLabel);
     liSun.appendChild(inputSun);
 
-    mySchedule.appendChild(liSun);
+    myInfo.appendChild(liSun);
 
 }
+
+let p6 = document.createElement("p");
+
+let node6 = document.createTextNode("Monday");
+
+p6.appendChild(node6);
+myInfo.appendChild(p6);
 let li2 = document.createElement('li');
 li2.appendChild(document.createTextNode("Monday"));
 let labelMonday = ["Breakfast", "Snack", "Lunch", "Snack", "Dinner"];
@@ -97,10 +89,15 @@ for (let i = 0; i< labelMonday.length; i++){
     liMon.appendChild(monLabel);
     liMon.appendChild(inputMon);
 
-    mySchedule.appendChild(liMon);
+    myInfo.appendChild(liMon);
 
 }
+let p7 = document.createElement("p");
 
+let node7 = document.createTextNode("Tuesday");
+
+p7.appendChild(node7);
+myInfo.appendChild(p7);
 
 
 
@@ -121,10 +118,15 @@ for (let i = 0; i< labelTuesday.length; i++){
     liTues.appendChild(tuesLabel);
     liTues.appendChild(inputTues);
 
-    mySchedule.appendChild(liTues);
+    myInfo.appendChild(liTues);
 
 }
+let p8 = document.createElement("p");
 
+let node8 = document.createTextNode("Wednesday");
+
+p8.appendChild(node8);
+myInfo.appendChild(p8);
 
 let li4 = document.createElement('li');
 li4.appendChild(document.createTextNode("Wednesday"));
@@ -143,9 +145,15 @@ for (let i = 0; i< labelWednesday.length; i++){
     liWeds.appendChild(wedsLabel);
     liWeds.appendChild(inputWeds);
 
-    mySchedule.appendChild(liWeds);
+    myInfo.appendChild(liWeds);
 
 }
+let p9 = document.createElement("p");
+
+let node9 = document.createTextNode("Thursday");
+
+p9.appendChild(node9);
+myInfo.appendChild(p9);
 
 
 let li5 = document.createElement('li');
@@ -165,10 +173,15 @@ for (let i = 0; i< labelThursday.length; i++){
     liThurs.appendChild(thursLabel);
     liThurs.appendChild(inputThurs);
 
-    mySchedule.appendChild(liThurs);
+    myInfo.appendChild(liThurs);
 
 }
+let p10 = document.createElement("p");
 
+let node10 = document.createTextNode("Friday");
+
+p10.appendChild(node10);
+myInfo.appendChild(p10);
 
 let li6 = document.createElement('li');
 li6.appendChild(document.createTextNode("Friday"));
@@ -187,10 +200,15 @@ for (let i = 0; i< labelFriday.length; i++){
     liFri.appendChild(friLabel);
     liFri.appendChild(inputFri);
 
-    mySchedule.appendChild(liFri);
+    myInfo.appendChild(liFri);
 
 }
+let p11 = document.createElement("p");
 
+let node11 = document.createTextNode("Saturday");
+
+p11.appendChild(node11);
+myInfo.appendChild(p11);
 
 let li7 = document.createElement('li');
 li7.appendChild(document.createTextNode("Saturday"));
@@ -209,7 +227,7 @@ for (let i = 0; i< labelSaturday.length; i++){
     liSat.appendChild(satLabel);
     liSat.appendChild(inputSat);
 
-    mySchedule.appendChild(liSat);
+    myInfo.appendChild(liSat);
 
 }
 ul.appendChild(li1);
@@ -220,21 +238,48 @@ ul.appendChild(li5);
 ul.appendChild(li6);
 ul.appendChild(li7);
 
-//mySchedule.appendChild(ul);
-function checkEmail(emailBox){
-    let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    //return regex.test(emailBox);
-    if (regex.test = true){
-        document.write(mySchedule)
-    } else {
-        window.alert("Invalid email address! Please try again.")
-    }
-}
+let break1= document.createElement("br");
+myInfo.appendChild(break1);
 
-submitButton.addEventListener("click", checkEmail()) ;
-    //{if (true){
-      //  window.alert("Invalid email address! Please try again.")
-    //} else {
-     //   document.write(mySchedule);
-//}
-//};
+let submit2 = document.createElement("button");
+submit2.innerText = "Submit"
+myInfo.appendChild(submit2);
+    
+let break2= document.createElement("br");
+myInfo.appendChild(break2);
+
+let reset = document.createElement("button");
+reset.innerText = "Clear"
+myInfo.appendChild(reset);
+
+let break3= document.createElement("br");
+myInfo.appendChild(break3);
+    
+let print = document.createElement("button");
+print.innerText = "Print";
+myInfo.appendChild(print)
+
+let break4= document.createElement("br");
+myInfo.appendChild(break4);
+
+let downLoad = document.createElement("button");
+downLoad.innerText = "Download"
+myInfo.appendChild(downLoad);
+
+submit2.addEventListener("click", newWeb)
+    }
+function newWeb(){
+    let planner = ""
+    for (let i=0; i<myInfo.children.length; i++){
+        let child = myInfo.children[i];
+        if (child.tagName ==="input"){
+        planner +=`${child.previousElementSibling.textContent}: ${child.value}<br>`;
+        }
+    }
+    //let mealPlan= document.getElementById("myInfo")
+    let newPage= window.open("", "_blank");
+    //newPage.document.write("Meal Plan");
+    //newPage.document.write(mealPlan)
+    newPage.document.write(planner);
+    }
+ 
